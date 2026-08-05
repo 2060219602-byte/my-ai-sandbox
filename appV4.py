bf7098d8a479c8befbebb8e3b2146251e8d4bfae
2060219602-byte <2060219602@qq.com>
2026-08-05 09:58:36 +0800

    Update appV4.py
    1

--- appV4.py
+++ appV4.py
@@ -2702,7 +2702,7 @@ else:
                         stream=True,
                         max_tokens=4000,
                         timeout=60.0,
-                        temperature=1.2,
+                        temperature=1.0,
                         presence_penalty=0.1,
                         frequency_penalty=0.1,
                         extra_body={"thinking": {"type": "disabled"}}
